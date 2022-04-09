ec48b6c9b119f98332f3518811e93b90b9c141d2
SerhiiCho <serhiicho@protonmail.com>
2022-04-09 14:13:09 +0300

    Parse is able to parse let statements now

--- parser/parser.go
+++ parser/parser.go
@@ -27,6 +27,80 @@ func (p *Parser) nextToken() {
 	p.peekToken = p.l.NextToken()
 }
 
+// It construct the root node of the AST, an *ast.Program.
+// It then iterates over every token in the input until it
+// encounters an token.EOF token. It does this by repeatedly
+// calling nextToken, which advances both p.curToken and
+// p.peekToken. In every iteration it calls parseStatement,
+// whose job it is to parse a statement. If parseStatement
+// returned something other than nil, a ast.Statement, its
+// return value is added to Statements slice of the AST root
+// node. When nothing is left to parse the *ast.Program root
+// node is returned.
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{
+		Token: p.curToken,
+		Value: p.curToken.Literal,
+	}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// We're skipping the expressions
+	// until we encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	}
+
+	return false
 }
