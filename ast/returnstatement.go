b3282485994dab87eaaea7d016b30b956ba16d91
SerhiiCho <serhiicho@protonmail.com>
2022-04-10 18:21:26 +0300

    Working on adding ast

--- ast/returnstatement.go
+++ ast/returnstatement.go
@@ -1,6 +1,10 @@
 package ast
 
-import "github.com/SerhiiCho/parser/token"
+import (
+	"bytes"
+
+	"github.com/SerhiiCho/parser/token"
+)
 
 type ReturnStatement struct {
 	Token       token.Token // the 'return' token
@@ -11,4 +15,18 @@ func (rs *ReturnStatement) statementNode() {}
 
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
 }
